1547dd4646743a3cc8d711c6c25f6bab92b4a5f9
abhishek konduri <abhishek.konduri@sjsu.edu>
2018-12-02 22:59:08 -0800

    Main Function for Payments Go Code

---
+++ Mongo Payment API/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"gopkg.in/mgo.v2/bson"
+	"github.com/gorilla/mux"
+	. "./ccs"
+)
+
+var ccs = CCSDB{}
+
+// API Ping Handler
+func testPing(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("testping")
+	respondWithJson(w, http.StatusOK, struct{ Test string }{"API version 1.0 alive!"})
+}
+
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func init() {
+	ccs.Connect()
+	ccs.ConnecttoPrimary()
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/ping", testPing).Methods("GET")
+	if err := http.ListenAndServe(":3001", r); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+
+
+
+
+
+
+
